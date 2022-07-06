25a828b617654c800feaab3670c998d3267a3b36
SaiGanesh18 <saiganeshm48@gmail.com>
2022-07-06 23:51:21 +0530

    First Commit

---
+++ js/music-list.js
@@ -0,0 +1,105 @@
+let allMusic = [
+    {
+        name: "Without Me",
+        artist:  "Eminem",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Toss a coin to your Witcher",
+        artist: "Yaskier",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Till I Collapse",
+        artist: "Eminem",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Enemy",
+        artist: "Imagine Dragons",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Believer",
+        artist: "Imagine Dragons",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Lose Yourself",
+        artist: "Eminem",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Sway",
+        artist: "Michael Buble",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "As it Was",
+        artist: "Harry Styles",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Sunflower",
+        artist: "Post Malone",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Thrift Shop",
+        artist: "Macklemore and Ryan Lewis",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Industry Baby ",
+        artist: "Lil Nas X",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "God's Plan",
+        artist: "Drake",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Rolling in the Deep",
+        artist: "Adele",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Pray for Me",
+        artist: "The Weeknd and Kendrick Lamar",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Mystery Lady",
+        artist: "Don Toliver and Masego",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Havana",
+        artist: "Camila Cabello",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Uptown Funk",
+        artist: "Bruno Mars",
+        img: "music-17",
+        src: "music-17"
+    }
+
+]
